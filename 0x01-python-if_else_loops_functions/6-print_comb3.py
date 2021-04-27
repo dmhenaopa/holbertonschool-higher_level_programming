35f30bf90deaebe77a4903dd6f584f7f56e242ff
Diana Henao <2777@holbertonschool.com>
2021-04-27 23:16:17 +0000

    :ambulance: Critical hotfix - remove the decimal specifiers of .format in print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range(1, 10):
         if i != j and i < j:
-            print("{:d}{:d}".format(i, j), end="")
+            print("{}{}".format(i, j), end="")
             if i != 8 or j != 9:
                 print(", ", end="")
