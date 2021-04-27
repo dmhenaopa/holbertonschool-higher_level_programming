cdca304c042bb2ffebc2604ac2dcb79f319a0786
Diana Henao <2777@holbertonschool.com>
2021-04-27 23:45:35 +0000

    :ambulance: Critical hotfix - revert the changes in .format with decimal specifiers and change the end in print format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range(1, 10):
         if i != j and i < j:
-            print("{}{}".format(i, j), end="")
+            print("{:d}{:d}".format(i, j), end="")
             if i != 8 or j != 9:
-                print(", ", end="")
+                print(",", end=" ")
