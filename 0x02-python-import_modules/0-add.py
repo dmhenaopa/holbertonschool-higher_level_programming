39eeefb0b9c74276f37e1032e8001196eea9d434
Diana Henao <2777@holbertonschool.com>
2021-04-28 17:32:17 +0000

    :ambulance: Critical hotfix - replace the use of add_0 into the code for function_add when import the module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import add_0
+import add_0 as function_add
 if __name__ == "__main__":
     # This only run when is not called via 'import'
     a = 1
     b = 2
 
-    value = add_0.add(a, b)
+    value = function_add.add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, value))
