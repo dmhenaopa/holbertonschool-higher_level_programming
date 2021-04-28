a4f1463dfc269c7b9f12f4e1414a229ed634f60e
Diana Henao <2777@holbertonschool.com>
2021-04-28 18:13:58 +0000

    :ambulance: Critical hotfix - change import as for from import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import add_0 as function_add
+from add_0 import add
 if __name__ == "__main__":
     # This only run when is not called via 'import'
     a = 1
     b = 2
 
-    print("{:d} + {:d} = {:d}".format(a, b, function_add.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
