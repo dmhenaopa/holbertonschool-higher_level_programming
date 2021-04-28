877e1590231730a968fd0e5a5a73069e5772a6f3
Diana Henao <2777@holbertonschool.com>
2021-04-28 21:05:40 +0000

    :hammer: Add development script 2-args.py and upload README.md file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    counter = 0
+    number_arguments = len(sys.argv) - 1
+    if number_arguments == 0:
+        print("{:d} arguments.".format(number_arguments))
+    elif number_arguments == 1:
+        print("{:d} argument:".format(number_arguments))
+    else:
+        print("{:d} arguments:".format(number_arguments))
+
+    for arg in sys.argv:
+        if counter < number_arguments:
+            counter = counter + 1
+            print("{:d}: {}".format(counter, sys.argv[counter]))
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -1,4 +1,5 @@
 A short description of each script:
 + 0-add.py: A program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3.
 + 1-calculation.py: A program that imports functions from the file calculator_1.py, does some maths, and prints the result.
++ 2-args.py: A program that prints the number of and the list of its arguments. The output is the number of arguments followed by one line per argument.
 +
