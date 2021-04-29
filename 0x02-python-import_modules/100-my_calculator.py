d76b71cb70f0cf443b73ecd58d088e5f51ca2392
Diana Henao <2777@holbertonschool.com>
2021-04-29 02:01:07 +0000

    :ambulance: Critical hotfix - remove the import sys and import specific the list of arguments of sys argv

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     # Verify number of arguments is equal to 4 - with command
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
         exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    symbol = sys.argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
+    symbol = argv[2]
 
     if symbol == '+' or symbol == '-' or symbol == '*' or symbol == '/':
-        if sys.argv[2] == '+':
+        if argv[2] == '+':
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
-        elif sys.argv[2] == '-':
+        elif argv[2] == '-':
             print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
 
-        elif sys.argv[2] == '*':
+        elif argv[2] == '*':
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
 
         else:
