727837110b81083e3d68c5594e1a7ef5d2da2410
Diana Henao <2777@holbertonschool.com>
2021-05-05 22:32:29 +0000

    :ambulance: Critical hotfix - change range with negative integers to the reverse() function

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list) - 1
-    for index in range(length, -1, -1):
+    length = len(my_list)
+    for index in reversed(range(length)):
         print("{:d}".format(my_list[index]))
