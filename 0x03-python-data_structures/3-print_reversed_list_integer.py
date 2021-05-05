5851d98137304d788c59acd0876c5d39db4a7555
Diana Henao <2777@holbertonschool.com>
2021-05-05 22:34:32 +0000

    :ambulance: Critical hotfix - change the range condition to length + 1

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
-    for index in reversed(range(length)):
+    for index in reversed(range(length + 1)):
         print("{:d}".format(my_list[index]))
