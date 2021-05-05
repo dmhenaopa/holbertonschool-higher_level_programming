27514c78ea1c6e33895fb09c871043a9f2e4cb30
Diana Henao <2777@holbertonschool.com>
2021-05-05 22:45:19 +0000

    :ambulance: Critical hotfix - try to resolve a problem with add the if-else conditional and return None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,3 +5,6 @@ def print_reversed_list_integer(my_list=[]):
     if my_list:
         for index in reversed(range(length)):
             print("{:d}".format(my_list[index]))
+
+    else:
+        return None
