6c4a590ede9873c69ad5c6184a1960fd38740925
Diana Henao <2777@holbertonschool.com>
2021-05-05 22:42:33 +0000

    :ambulance: Critical hotfix - put condition to check if my_list exist

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
 
-    if length > 0:
+    if my_list:
         for index in reversed(range(length)):
             print("{:d}".format(my_list[index]))
