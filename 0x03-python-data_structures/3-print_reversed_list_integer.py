08fdb9527ce844e2394a51809ec46b26ae36e405
Diana Henao <2777@holbertonschool.com>
2021-05-05 22:37:35 +0000

    :rewind: Revert changes and add conditional in case the list is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
-    for index in reversed(range(length + 1)):
-        print("{:d}".format(my_list[index]))
+
+    if length > 0:
+        for index in reversed(range(length)):
+            print("{:d}".format(my_list[index]))
