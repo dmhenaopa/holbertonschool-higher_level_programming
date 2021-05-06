98c9d2e67efa01749a4f89e97d1deee66e3ff285
Diana Henao <2777@holbertonschool.com>
2021-05-06 03:05:50 +0000

    :poop: Bad code to be improved - remove lines with conditional to know if the tuples exist

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,36 +4,32 @@ def add_tuple(tuple_a=(), tuple_b=()):
     length_b = len(tuple_b)
 
     new_tuple = (0, 0)
-    if tuple_a or tuple_b:
-        if length_a >= 2 and length_b >= 2:
-            new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
-        else:
-            if length_a == 2 and length_b == 1:
-                new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
-
-            elif length_a == 2 and length_b == 0:
-                new_tuple = (tuple_a[0] + 0, tuple_a[1] + 0)
-
-            elif length_a == 1 and length_b == 2:
-                new_tuple = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
-
-            elif length_a == 1 and length_b == 1:
-                new_tuple = (tuple_a[0] + tuple_b[0], 0 + 0)
-
-            elif length_a == 1 and length_b == 0:
-                new_tuple = (tuple_a[0] + 0, 0 + 0)
-
-            elif length_a == 0 and length_b == 2:
-                new_tuple = (0 + tuple_b[0], 0 + tuple_b[1])
-
-            elif length_a == 0 and length_b == 1:
-                new_tuple = (0 + tuple_b[0], 0 + 0)
-
-            elif length_a <= 0 and length_b <= 0:
-                new_tuple = (0 + 0, 0 + 0)
+    if length_a >= 2 and length_b >= 2:
+        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
 
     else:
-        return None
+        if length_a == 2 and length_b == 1:
+            new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+
+        elif length_a == 2 and length_b == 0:
+            new_tuple = (tuple_a[0] + 0, tuple_a[1] + 0)
+
+        elif length_a == 1 and length_b == 2:
+            new_tuple = (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
+
+        elif length_a == 1 and length_b == 1:
+            new_tuple = (tuple_a[0] + tuple_b[0], 0 + 0)
+
+        elif length_a == 1 and length_b == 0:
+            new_tuple = (tuple_a[0] + 0, 0 + 0)
+
+        elif length_a == 0 and length_b == 2:
+            new_tuple = (0 + tuple_b[0], 0 + tuple_b[1])
+
+        elif length_a == 0 and length_b == 1:
+            new_tuple = (0 + tuple_b[0], 0 + 0)
+
+        elif length_a <= 0 and length_b <= 0:
+            new_tuple = (0 + 0, 0 + 0)
 
     return new_tuple
