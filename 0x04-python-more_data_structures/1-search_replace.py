421ba7d59053e19189e41ce717a9de38df4c2c95
Diana Henao <2777@holbertonschool.com>
2021-05-06 22:12:57 +0000

    :hammer: Add development script - 1-search_replace.py and upload README.md file

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+
+    for index in range(len(new_list)):
+        if new_list[index] == search:
+            new_list[index] = replace
+
+    return new_list
