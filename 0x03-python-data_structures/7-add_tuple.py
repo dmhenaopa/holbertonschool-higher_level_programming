ee01134fddef1415796e50bfba4e964ae3bef9ad
Diana Henao <2777@holbertonschool.com>
2021-05-06 02:59:32 +0000

    :poop: Modifications to a bad code - remove a code line that has conflict and produce errors in return

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,11 +4,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
     length_b = len(tuple_b)
 
     new_tuple = (0, 0)
-    if length_a >= 2 and length_b >= 2:
-        new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    if tuple_a or tuple_b:
+        if length_a >= 2 and length_b >= 2:
+            new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
 
-    else:
-        if length_a == 1 or length_b == 1:
+        else:
             if length_a == 2 and length_b == 1:
                 new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
 
@@ -32,5 +32,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
             elif length_a <= 0 and length_b <= 0:
                 new_tuple = (0 + 0, 0 + 0)
+
+    else:
+        return None
 
     return new_tuple
