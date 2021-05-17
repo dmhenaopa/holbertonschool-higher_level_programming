530eef412de6daec7cb6349b5b213c763f6422d6
Diana Henao <2777@holbertonschool.com>
2021-05-17 17:10:00 +0000

    :adhesive_bandage: Simple fix for a non-critical issue. Change single quotation mark to double

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@
 def safe_print_integer(value):
     try:
         if isinstance(value, int):
-            print('{:d}'.format(value))
+            print("{:d}".format(value))
             return True
 
     except TypeError as exc:
