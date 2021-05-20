414acc2853e4d144f873c239f9a4b493d39107ea
Diana Henao <2777@holbertonschool.com>
2021-05-20 15:58:44 +0000

    :hammer: Add development script 0-rectangle.py and upload README file

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module has a class Rectangle"""
+
+
+class Rectangle:
+    """An empty class that defines a rectangle"""
+    pass
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -1,2 +1,3 @@
 Short description of each script:
++ 0-rectangle.py: An empty class Rectangle that defines a rectangle.
 +
