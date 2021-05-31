cf5bd47b33c9f71cc726eed31c2e520e530d9ef8
Diana Henao <2777@holbertonschool.com>
2021-05-31 22:28:29 +0000

    :ambulance: Critical hotfix - add w+

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,6 +9,6 @@ def write_file(filename="", text=""):
             param1 (filename): Path to file
             param2 (text): string to write in the file
     """
-    with open(filename, mode='w', encoding='utf-8') as my_file:
+    with open(filename, mode='w+', encoding='utf-8') as my_file:
         number_char = my_file.write(text)
         return number_char
