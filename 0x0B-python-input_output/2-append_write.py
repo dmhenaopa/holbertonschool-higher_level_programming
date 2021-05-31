624412d4b60ee47a48fe859abecd4b4a1f7ba995
Diana Henao <2777@holbertonschool.com>
2021-05-31 20:38:01 +0000

    :hammer: Add development script 2-append_write.py and upload README.md file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def append_write(filename="", text=""):
+    """Function that appends a string at the end of
+       a text file
+
+       Args:
+            param1 (filename): Path to file
+            param2 (text): String to add
+    """
+    with open(filename, mode='a', encoding='utf-8') as my_file:
+        number_char = my_file.write(text)
+        return number_char
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -1,4 +1,5 @@
 A short description of each script:
 + 0-read_file.py: A function that reads a text file (UTF8) and prints it to stdout.
 + 1-write_file.py: A function that writes a string to a text file (UTF8) and returns the number of characters written.
++ 2-append_write.py: A function that appends a string at the end of a text file (UTF8) and returns the number of characters added.
 +
