d26d30dd16563f5bd7b4299364bff2ffe9ef6265
Diana Henao <2777@holbertonschool.com>
2021-05-31 22:36:20 +0000

    :ambulance: Critical hotfix - add mode r

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""Function that reads a text file"""
 
 
 def read_file(filename=""):
-    with open(filename, encoding='utf-8') as my_file:
+    """
+       Function that reads a text file
+       Args:
+            param1 (filename): Path to file
+    """
+    with open(filename, mode='r', encoding='utf-8') as my_file:
         read_data = my_file.read()
         print("{}".format(read_data), end=" ")
