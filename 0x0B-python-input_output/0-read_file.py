2063a106f91ea91d0e8d7ec903e94980463d3a3d
Diana Henao <2777@holbertonschool.com>
2021-05-31 21:55:16 +0000

    :ambulance: Critical hotfix - remove the comments in the function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,12 +2,6 @@
 
 
 def read_file(filename=""):
-    """
-       Function that reads a text file
-
-       Args:
-            param1 (filename): Path to file
-    """
     with open(filename, encoding='utf-8') as my_file:
         read_data = my_file.read()
         print("{}".format(read_data), end=" ")
