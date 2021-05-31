93fa2d2a5ace5664cf70dbe1be25cecafee43d39
Diana Henao <2777@holbertonschool.com>
2021-05-31 22:38:55 +0000

    :ambulance: Critical hotfix - remove mode r

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
        Args:
             param1 (filename): Path to file
     """
-    with open(filename, mode='r', encoding='utf-8') as my_file:
+    with open(filename, encoding='utf-8') as my_file:
         read_data = my_file.read()
-        print("{}".format(read_data), end=" ")
+        print(read_data, end="")
