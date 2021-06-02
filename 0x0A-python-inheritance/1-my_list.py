aa493dff54ad40880fd723fd863f29098ec04eea
Diana Henao <2777@holbertonschool.com>
2021-06-02 23:15:48 +0000

    :ambulance: Critical hotfix - remove a raise TypeError from 1-my_list.py and change the results that must be obtained from the test file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -13,10 +13,6 @@ class MyList(list):
         """
            Method to sort and prints the list in ascending order
         """
-
-        if type(self) is not list:
-            raise TypeError('the argument must be a list')
-
         new_list = self[:]
         new_list.sort()
         print(new_list)
