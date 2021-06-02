d0b8316e0e2e6fa22840c007fa935a909815386f
Diana Henao <2777@holbertonschool.com>
2021-06-02 22:11:24 +0000

    hammer: Add development script 1-my_list.py, test file 1-my_list.txt and upload README file

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module that contains the class MyList"""
+
+
+class MyList(list):
+    """
+       Class MyList that has a method to sort the list
+
+       Attributes:
+       list: As an external argument
+    """
+    def print_sorted(self):
+        """
+           Method to sort and prints the list in ascending order
+        """
+
+        if type(self) is not list:
+            raise TypeError('the argument must be a list')
+        
+        new_list = self[:]
+        new_list.sort()
+        print(new_list)
--- 0x0A-python-inheritance/README.md
+++ 0x0A-python-inheritance/README.md
@@ -1,3 +1,4 @@
 A short description of each file:
 + 0-lookup.py: A function that returns the list of available attributes and methods of an object.
++ 1-my_list.py: A class MyList that inherits from list.
 +
