a11be42cf30f223ad8daab231981837e34314a09
Diana Henao <2777@holbertonschool.com>
2021-06-08 18:18:11 +0000

    :hammer: Add development script base.py and __init__.py empty file (Python package)

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Module that contains the class Base"""
+import json
+
+
+class Base:
+    """
+       A parent class named Base
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+           Constructor method __init__ to initialize the Base instance
+
+           Args:
+                param1 (id): Id of the object
+        """
+        self.__nb_objects = 0
+
+        if id is not None:
+            self.id = id
+
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    """--------------------------METHODS-------------------------"""
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+            Returns the JSON string representation to
+            list_dictionaries
+
+            Args:
+                 param1 (list_dictionaries): List of dictionaries
+        """
+        string = "[]"
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return string
+
+        string = json.dumps(list_dictionaries)
+        return string
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+            Writes the JSON string representation of list_objs to a file
+
+            Args:
+                 param1 (list_objs): List of instances who inherits of Base
+        """
+        list_objects = []
+        filename = "{}.json".format(cls.__name__)
+        if list_objs is None:
+            list_objects = []
+
+        else:
+            for object in list_objs:
+                list_objects.append(cls.to_dictionary(object))
+
+        with open(filename, mode='w', encoding='utf-8') as my_file:
+            json.dump(list_objects, my_file)
