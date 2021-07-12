2f100d44968a48a45537471cb6bb146761e3a7d9
Diana Henao <2777@holbertonschool.com>
2021-07-12 22:04:15 +0000

    :ambulance: Critical hotfix - change the COUNT(*) keyword and add WHERE keyword with id

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- Displays the number of records with id = 89 in the table
 -- first_table of the database hbtn_0c_0 in your MySQL server.
-SELECT COUNT('89') FROM first_table;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
