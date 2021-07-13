deb5ada67ad3685a7d89c144d9469b63b50059dd
Diana Henao <2777@holbertonschool.com>
2021-07-13 21:30:57 +0000

    :ambulance: Critical hotfix - removes PRIMARY KEY of id

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,7 +6,7 @@
 -- the states table; name VARCHAR(256) can’t be null.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+id INT NOT NULL AUTO_INCREMENT UNIQUE,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL),
 FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
