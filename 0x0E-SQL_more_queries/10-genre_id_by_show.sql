07331b132dd8546427791d4725949591c1b5f925
Diana Henao <2777@holbertonschool.com>
2021-07-13 23:09:21 +0000

    :hammer: Add development script 10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id.
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id.
+SELECT tv_shows.title,tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
