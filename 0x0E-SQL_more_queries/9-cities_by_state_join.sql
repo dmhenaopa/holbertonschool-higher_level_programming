b90f44a900f83b135042a33cb6bafbf1d9f7faea
Diana Henao <2777@holbertonschool.com>
2021-07-13 22:41:13 +0000

    :hammer: Add development script 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,4 @@
 -- Each record should display: cities.id - cities.name - states.name.
 -- Results must be sorted in ascending order by cities.id.
 SELECT cities.id,cities.name,states.name FROM cities INNER JOIN states
-ON cities.state_id = state.id ORDER BY cities.id ASC;
+ON cities.state_id = states.id ORDER BY cities.id ASC;
