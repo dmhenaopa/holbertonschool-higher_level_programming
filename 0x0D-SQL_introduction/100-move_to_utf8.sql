f1ee17872d0aef8ee295bb47edb38d244edeabb8
Diana Henao <2777@holbertonschool.com>
2021-07-14 03:19:58 +0000

    :hammer: Add development script 100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- Convert all of the following to UTF8:
+-- Database hbtn_0c_0, Table first_table and Field name in first_table
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
