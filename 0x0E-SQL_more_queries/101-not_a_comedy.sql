fb4df33994389e56841a3d37fe65d21c7f6558eb
Diana Henao <2777@holbertonschool.com>
2021-07-15 03:48:28 +0000

    :hammer: Add development script 101-not_a_comedy.sql

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,15 @@
+-- Script that lists all shows without the genre Comedy 
+-- in the database hbtn_0d_tvshows
+SELECT DISTINCT tv_shows.title
+	AS `title`
+	FROM tv_genres RIGHT JOIN tv_show_genres
+	ON tv_genres.id = tv_show_genres.genre_id RIGHT JOIN tv_shows
+	ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title  NOT IN (
+		SELECT tv_shows.title
+			AS `title`
+			FROM tv_genres INNER JOIN tv_show_genres
+			ON tv_genres.id = tv_show_genres.genre_id INNER JOIN tv_shows
+			ON tv_show_genres.show_id = tv_shows.id
+			WHERE tv_genres.name = "Comedy")
+	ORDER BY title ASC;
