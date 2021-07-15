863858661ffdcae474c8cb890d058e513e356951
Diana Henao <2777@holbertonschool.com>
2021-07-15 03:40:31 +0000

    :ambulance: Critical hotfix - change statement SELECT to SELECT DISTINCT to obtain unique records

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,6 +1,6 @@
--- Script that uses the hbtn_0d_tvshows database to 
--- lists all genres of the show Dexter.
-SELECT tv_genres.name
+-- Script that uses the hbtn_0d_tvshows database 
+-- to list all genres not linked to the show Dexter.
+SELECT DISTINCT tv_genres.name
 	AS `name`
 	FROM tv_genres INNER JOIN tv_show_genres
 	ON tv_genres.id = tv_show_genres.genre_id INNER JOIN tv_shows
