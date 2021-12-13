40babec82bb92d475835833e7ced59ecd6c23204
Diana Henao <dimhenaopa@gmail.com>
2021-12-13 19:11:15 +0000

    :hammer: Add development script 1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const languages = ['C', 'Python', 'JavaScript'];
+const adjectives = ['fun', 'cool', 'amazing'];
+
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i] + ' is ' + adjectives[i]);
+}
