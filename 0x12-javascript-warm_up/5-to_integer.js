e3f02e111904e48584b09fd07d7cfd966552298c
Diana Henao <dimhenaopa@gmail.com>
2021-12-13 20:38:08 +0000

    :hammer: Add development script 5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv;
+
+if (isNaN(args[2])) {
+  console.log('Not a number');
+} else {
+  let number = parseInt(args[2]);
+  console.log('My number: ' + number);
+}
