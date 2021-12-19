dd875f3fd111f4a26f1899e4b142a4d353c54fee
Diana Henao <2777@holbertonschool.com>
2021-12-19 17:00:39 +0000

    :hammer: Add development script 7-model_state_fetch_all.py and SQL script 7-model_state_fetch_all.sql

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+   Module with script that lists all State objects
+   from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+hostname = "localhost"
+number_port = 3306
+username = argv[1]
+password = argv[2]
+database_name = argv[3]
+
+engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                       .format(username,
+                               password,
+                               hostname,
+                               database_name))
+Base.metadata.create_all(engine)
+Session = sessionmaker(engine)
+session = Session()
+
+if __name__ == '__main__':
+    """SELECT * FROM states ORDER BY id ASC;"""
+    states = session.query((State).order_by(State.id))
+
+    for state in states:
+        print(print("{}: {}".format(state.id, state.name)))
+
+    session.close()
