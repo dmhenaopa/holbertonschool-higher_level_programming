dca1c83d5e0760977a2c60b4be58fdc0116edc7c
Diana Henao <2777@holbertonschool.com>
2021-12-19 11:04:51 +0000

    :hammer: Add development scripts 6-model_state.py, model_state.py and 6-model_state.sql script

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+   Module that contains the class definition of a States
+   and an instance Base = declarative_base()
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Definition of class State that inherits from Base class"""
+    __tablename__ = 'states'
+
+    id = Column(Integer(), unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
