4f0d82ed5ac54a83b4a561bf92970c7e87c8a966
Diana Henao <2777@holbertonschool.com>
2021-12-20 02:04:49 +0000

    :hammer: Add development script 11-model_state_insert.py

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+   Module with script that adds the State object "Louisiana"
+   to the database hbtn_0e_6_usa
+"""
+from sys import argv
+from model_state import Base, State
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    number_port = 3306
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    state_name = argv[4]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, database_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(state_name)
+    session.add(new_state)
+    print("{}".format(state.id))
+
+    session.commit()
+    session.close()
