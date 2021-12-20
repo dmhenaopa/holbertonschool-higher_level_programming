f6a519dc96be4a2bd1a99fdf882ff033c5f381d0
Diana Henao <2777@holbertonschool.com>
2021-12-20 03:31:23 +0000

    :ambulance: Critical hotfix - addition of for loop to delete all the objects

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -22,6 +22,10 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    session.query(State).filter(State.name.contains("a")).delete()
+    states = session.query(State).filter(State.name.contains("a"))
+
+    for state in states:
+        session.delete(state)
+
     session.commit()
     session.close()
