756be61e604e54591787446ed8a3afcb1ed40431
Diana Henao <2777@holbertonschool.com>
2021-12-20 02:29:57 +0000

    :ambulance: Critical hotfix - remove innecesary query condition

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -26,9 +26,7 @@ if __name__ == "__main__":
     new_state = State(name=state_name)
     session.add(new_state)
 
-    state = session.query(State).filter(State.name == state_name).first()
-    if state is not None:
-        print("{}".format(state.id))
+    print("{}".format(new_state.id))
 
     session.commit()
     session.close()
