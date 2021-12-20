cff697ccf27d1b1ddd007a0f0e7536511b45259e
Diana Henao <2777@holbertonschool.com>
2021-12-20 03:24:48 +0000

    :hammer: Add development script 13-model_state_delete_a.py

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+   Module with script that deletes all State objects
+   that contain the letter a from the database hbtn_0e_6_usa
+"""
+from sys import argv
+from model_state import Base, State
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    number_port = 3306
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, database_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(State).filter(State.name.contains("a")).delete()
+    session.commit()
+    session.close()
