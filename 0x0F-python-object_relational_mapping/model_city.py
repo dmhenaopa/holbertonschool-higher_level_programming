405e89f30f23f07a49d76e58ba4fea082c1a85e6
Diana Henao <2777@holbertonschool.com>
2021-12-20 03:51:35 +0000

    :hammer: Add development scripts model_city.py, 14-model_city_fetch_by_state.py and sql script 14-model_city_fetch_by_state.sql

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+   Module that contains the class definition of a City
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """Definition of class City that inherits from Base class"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer(), unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer(), nullable=False, ForeignKey('states.id'))
