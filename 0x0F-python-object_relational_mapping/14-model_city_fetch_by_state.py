3bd3da2ba1ca3c2a97a45d04cc3332425ba4cd05
Diana Henao <2777@holbertonschool.com>
2021-12-20 04:06:58 +0000

    :ambulance: Critical hotfix - change query condition to include State information

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -23,7 +23,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(City).order_by(City.id).all()
+    cities = session.query(State, City).filter(State.id == City.id)\
+                    .order_by(City.id).all()
 
     for city in cities:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
