15e15e7140c57311360b981016533421d54a0340
Diana Henao <dimhenaopa@gmail.com>
2021-12-30 18:15:38 -0800

    :hammer: Add development script 101-relationship_states_cities_list.py and MySQL script 101-relationship_states_cities_list.sql

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+   Script that creates the State" California" with the City "San Francisco"
+   from the database hbtn_0e_100_usa
+"""
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    number_port = 3306
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, database_name))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id)
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
+
+    session.close()
