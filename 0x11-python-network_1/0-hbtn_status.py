71d0d3e6bb3973f02e67f1ab92c9edede1e64d77
Diana Henao <dimhenaopa@gmail.com>
2022-01-09 19:46:54 +0000

    :alembic: Perform experiments - change import statement

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status"""
-from urllib import request
+import urllib.request as request
 
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
         fetch_html = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(fetch_html)))
