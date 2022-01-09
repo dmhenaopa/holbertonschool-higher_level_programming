82248d0b1f7ad8e14e8d5226ce6259b14be39be6
Diana Henao <dimhenaopa@gmail.com>
2022-01-09 05:23:35 +0000

    :hammer: Update script 4-header.sh, remove a part of name of variable

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that takes in a URL, with a header variable X-School-User-Id value 98
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -H "X-School-User-Id: 98" "$1"
