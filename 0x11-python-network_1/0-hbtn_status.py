42da0f7dad7e16294c436ae64834cc0f643a3fd7
Diana Henao <dimhenaopa@gmail.com>
2022-01-09 19:36:02 +0000

    :adhesive_bandage: Simple fix for a non-critical issue - remove an unnecesary import statement

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status"""
 from urllib import request
-from urllib.request import unquote
+
 
 if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as response:
