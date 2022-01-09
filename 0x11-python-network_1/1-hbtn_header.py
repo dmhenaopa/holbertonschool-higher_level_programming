95f59d241ef24f15232454d79f0d8cce633ddf39
Diana Henao <dimhenaopa@gmail.com>
2022-01-09 17:22:04 +0000

    :bug: Fix a bug - need to take an URL from arguments passed

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status"""
 from urllib import request
-
+from sys import argv
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
+    with request.urlopen(argv[1]) as response:
         print(response.info()["X-Request-Id"])
