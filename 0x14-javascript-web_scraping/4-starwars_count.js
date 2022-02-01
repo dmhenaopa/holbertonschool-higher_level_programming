dc42918e2f06e1ddb9a0f4a9778f5b1dcb9c79d4
Diana Henao <dimhenaopa@gmail.com>
2022-02-01 21:44:04 +0000

    :hammer: Add development script 4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+const request = require('request');
+const argumentsArray = process.argv;
+
+const url = argumentsArray[2];
+let counter = 0;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    if (response && response.statusCode === 200) {
+      const information = JSON.parse(body);
+      for (const [key, value] of Object.entries(information)) {
+        if (key === 'results') {
+          value.forEach(function (element, index, array) {
+            const characters = element.characters;
+            for (const [, valuechar] of Object.entries(characters)) {
+              if (valuechar === 'https://swapi-api.hbtn.io/api/people/18/') {
+                counter++;
+              }
+            }
+          });
+          console.log(counter);
+        }
+      }
+    }
+  }
+});
