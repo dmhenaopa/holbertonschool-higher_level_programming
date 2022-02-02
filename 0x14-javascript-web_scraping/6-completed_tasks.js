b88a85d29b714ee3e0c3f0954be39bfb86f0d113
Diana Henao <dimhenaopa@gmail.com>
2022-02-02 03:23:37 +0000

    :alembic: Perform experiments - remove first for (unnecesary)

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -11,10 +11,6 @@ request(url, function (error, response, body) {
     const information = JSON.parse(body);
 
     for (let i = 0; i < information.length; i++) {
-      object[information[i].userId] = 0;
-    }
-
-    for (let i = 0; i < information.length; i++) {
       if (information[i].completed) {
         if (object[information[i].userId] === undefined) {
           object[information[i].userId] = 1;
