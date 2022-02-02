28a7a0db94f39c5a1ec4ade4010131b6d331ccb3
Diana Henao <dimhenaopa@gmail.com>
2022-02-02 02:41:24 +0000

    :hammer: Add development script 6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require('request');
+const argumentsArray = process.argv;
+
+const url = argumentsArray[2];
+const object = {};
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    if (response && response.statusCode === 200) {
+      const information = JSON.parse(body);
+      for (let i = 0; i < information.length; i++) {
+        object[information[i].userId] = 0;
+      }
+
+      for (let i = 0; i < information.length; i++) {
+        if (information[i].completed) {
+          object[information[i].userId]++;
+        }
+      }
+      console.log(object);
+    } else {
+      console.log('code:', response && response.statusCode);
+    }
+  }
+});
