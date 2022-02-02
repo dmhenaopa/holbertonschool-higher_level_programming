c602861e1b64d4459fe193bd294817a4e8a63dae
Diana Henao <dimhenaopa@gmail.com>
2022-02-02 03:51:44 +0000

    :hammer: Add development script 100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+const request = require('request');
+const argumentsArray = process.argv;
+
+const movieID = argumentsArray[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    if (response && response.statusCode === 200) {
+      const information = JSON.parse(body);
+      const characters = information.characters;
+
+      for (let i = 0; i < characters.length; i++) {
+        request(characters[i], function (error, response, body) {
+          if (error) {
+            console.error('error:', error);
+          } else {
+            if (response && response.statusCode === 200) {
+              const informationCharacter = JSON.parse(body);
+              console.log(informationCharacter.name);
+            }
+          }
+        });
+      }
+    }
+  }
+});
